d6f62706e5afcb6a05b09b91dae37d6690520871
Keane Quibilan <arv.r2vq@gmail.com>
2019-02-25 19:40:40 -0500

    Move Main Activity into activity package for organization.

--- app/src/main/java/com/keanequibilan/githubexplorer/activity/MainActivity.kt
+++ app/src/main/java/com/keanequibilan/githubexplorer/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.keanequibilan.githubexplorer
+package com.keanequibilan.githubexplorer.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.keanequibilan.githubexplorer.R
 
 class MainActivity : AppCompatActivity() {
 
