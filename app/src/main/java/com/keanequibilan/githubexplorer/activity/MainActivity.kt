3fa15a23198db7e5f9bc59ed8eaf6998eb7ad591
Keane Quibilan <arv.r2vq@gmail.com>
2019-02-25 19:41:52 -0500

    Add GitHub view model as a dependency for MainActivity.

--- app/src/main/java/com/keanequibilan/githubexplorer/activity/MainActivity.kt
+++ app/src/main/java/com/keanequibilan/githubexplorer/activity/MainActivity.kt
@@ -1,13 +1,34 @@
 package com.keanequibilan.githubexplorer.activity
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import com.keanequibilan.githubexplorer.R
+import com.keanequibilan.githubexplorer.model.User
+import com.keanequibilan.githubexplorer.viewmodel.GitHubViewModel
+import kotlinx.android.synthetic.main.activity_main.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val gitHubViewModel: GitHubViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        gitHubViewModel
+            .getUser()
+            .observe(this, Observer { setUser(it) })
+
+        btn_search
+            .setOnClickListener {
+                gitHubViewModel.loadUser(et_search.text.toString())
+            }
+    }
+
+    private fun setUser(user: User) {
+        Toast.makeText(this, "User found: ${user.name}", Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/keanequibilan/githubexplorer/di/AppModule.kt
+++ app/src/main/java/com/keanequibilan/githubexplorer/di/AppModule.kt
@@ -2,9 +2,13 @@ package com.keanequibilan.githubexplorer.di
 
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.keanequibilan.githubexplorer.network.RetrofitClient
+import com.keanequibilan.githubexplorer.viewmodel.GitHubViewModel
+import kotlinx.coroutines.Dispatchers
 import okhttp3.OkHttpClient
+import org.koin.androidx.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
 import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.coroutines.CoroutineContext
 
 val APP_MODULE = module {
     /**
@@ -27,5 +31,15 @@ val APP_MODULE = module {
      * Provides the [GsonConverterFactory].
      */
     single { GsonConverterFactory.create() }
+
+    /**
+     * Provides a [CoroutineContext] specifically for IO tasks.
+     */
+    single<CoroutineContext>("backgroundContext") { Dispatchers.IO }
+
+    /**
+     * Provides the [GitHubViewModel]. Depends on [RetrofitClient] and a background [CoroutineContext].
+     */
+    viewModel { GitHubViewModel(get(), get("backgroundContext")) }
 
 }
