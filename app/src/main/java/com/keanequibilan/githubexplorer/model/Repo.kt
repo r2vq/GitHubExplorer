8681ea5c38cfe7fb1908e5aab733cf74216cd244
Keane Quibilan <arv.r2vq@gmail.com>
2019-02-26 00:21:21 -0500

    Fix bug caused by incorrect key.

--- app/src/main/java/com/keanequibilan/githubexplorer/model/Repo.kt
+++ app/src/main/java/com/keanequibilan/githubexplorer/model/Repo.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 data class Repo(
     @SerializedName("name") val name: String,
     @SerializedName("description") val description: String,
-    @SerializedName("update_at") val updateAt: String,
+    @SerializedName("updated_at") val updatedAt: String,
     @SerializedName("stargazers_count") val stargazersCount: Int,
     @SerializedName("forks") val forks: Int
 )
