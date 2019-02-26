6b436a2b1c3b6c33e8b62fb8046071d57ee24829
Keane Quibilan <arv.r2vq@gmail.com>
2019-02-25 19:37:29 -0500

    Retrieve a Response instead of a User object to prevent crashing when call is not successful.

--- app/src/main/java/com/keanequibilan/githubexplorer/network/GitHubService.kt
+++ app/src/main/java/com/keanequibilan/githubexplorer/network/GitHubService.kt
@@ -2,10 +2,11 @@ package com.keanequibilan.githubexplorer.network
 
 import com.keanequibilan.githubexplorer.model.User
 import kotlinx.coroutines.Deferred
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface GitHubService {
     @GET("/users/{name}")
-    fun getUsersAsync(@Path("name") name: String): Deferred<User>
+    fun getUsersAsync(@Path("name") name: String): Deferred<Response<User>>
 }
